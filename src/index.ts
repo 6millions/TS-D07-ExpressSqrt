0d8a868ebe82d0686d1251a79bfc131d68d249a0
6millions <kittichai.s@ic-connection.com>
2024-05-18 22:49:27 +0700

    finish challenge for today

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import { Complex, number, sqrt } from 'mathjs';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +11,48 @@ app.use(bodyParser.json());
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, TypeScript with Express!');
 });
+
+app.post('/sqrt', (req: Request, res: Response) => {
+
+    const { input } = req.body;
+
+    if (input === undefined) {
+        return res.status(400).send('Invalid input');
+    }
+
+    const result = calculateSquareRoot(input);
+
+    if (typeof result === 'number') {
+        return res.json({ result });
+    } else {
+        return res.status(400).send(result);
+    }
+
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+function calculateSquareRoot(input: number): number | string {
+
+
+
+    if (typeof input === 'number') {
+        const result = sqrt(input);
+
+        if (typeof result === 'number' && !isNaN(result)) {
+
+            return result;
+        }
+        else {
+
+            return 'Invalid input';
+        }
+
+    } else {
+        return 'Invalid input';
+    }
+}
